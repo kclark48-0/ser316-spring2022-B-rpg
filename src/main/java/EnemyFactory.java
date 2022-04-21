bfc8b047d9f44fee3644163ab0cce50e9ef6ef5e
Kevin-Clarke <97864915+Kevin-Clarke@users.noreply.github.com>
2022-04-20 21:20:27 -0700

    Implemented basic enemy scaling based on dungeon level

--- src/main/java/EnemyFactory.java
+++ src/main/java/EnemyFactory.java
@@ -1,15 +1,21 @@
 public class EnemyFactory {
     Dungeon dungeon;
+    public final double baseHealth = 5;
+    public final double baseMana = 2;
+    public final double baseAttack = 1;
+    public final double baseDefense = 1;
+    public final double baseSpeed = 1;
+    public final double baseReflex = 1;
+    public final double levelScaling = 5;
 
     public EnemyFactory(){
         dungeon = Dungeon.getInstance();
     }
 
     public Combatant createEnemy(String type){
-        double scaling;
+        double scaling = 1;
         switch (type) {
             case "mob":
-                scaling = 1;
                 break;
             case "miniboss":
                 scaling = 1.5;
@@ -19,8 +25,13 @@ public class EnemyFactory {
                 break;
         }
         String name = "default";
-        int level = dungeon.currentFloor/5;
-
-        Enemy newEnemy = new Enemy("default", dungeon.currentFloor/5, );
+        int level = (int) (dungeon.currentFloor/levelScaling);
+        int maxHealth = (int) (baseHealth * level * scaling);
+        int maxMana = (int) (baseMana * level);
+        int attack = (int) (baseAttack * level);
+        int defense = (int) (baseDefense * level);
+        int speed = (int) (baseSpeed * level);
+        int reflex = (int) (baseReflex * level);
+        return new Enemy(name, level, maxHealth, maxMana, attack, defense, speed, reflex);
     }
 }
